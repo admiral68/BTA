b71b1410c1fef65a4f87708c87c27d0df0ed6caa
chadderack <icbiass@hotmail.com>
2021-09-25 09:06:53 -0600

    "Hello World!" (or "Sky") working (heavy thanks to Photon of ScoopEx)

--- main.asm
+++ main.asm
@@ -1,12 +1,222 @@
-	INCDIR ""
-	INCLUDE "photon/PhotonsMiniWrapper1.04!.S"
-	INCLUDE "photon/Blitter-Register-List.S"
-	
+    INCDIR ""
+    INCLUDE "photon/PhotonsMiniWrapper1.04!.S"
+    INCLUDE "photon/Blitter-Register-List.S"
+
+*******************************************************************************
+* DEFINES
+*******************************************************************************
+
+w               =320
+h               =256
+bplsize         =w*h/8
+
+bpls            =3                      ;handy values:
+bpl             =w/16*2                 ;byte-width of 1 bitplane line
+bwid            =bpls*bpl               ;byte-width of 1 pixel line (all bpls)
+
+    *-----------------*
+	* logo dimensions *
+    *-----------------*
+
+logow           =320
+logomargin      =(320-logow)/2
+logobpl         =logow/8
+logobwid        =logobpl*3
+
+logobgcol       =$44f
+
+    *-----------------*
+	* registers       *
+    *-----------------*
+
+
+INTREQR         = $1F
+COP1LCH         = $80
+DIWSTRT         = $8E
+DIWSTOP         = $90
+INTENA          = $9A
+INTREQ          = $9C
+
+
+*******************************************************************************
+* MACROS
+*******************************************************************************
+
+logocolors:macro
+    dc.w $068e,$0adf,$0dff
+    dc.w $09bf,$056d,$044b,$033a
+    endm
+
+WAITBLIT:macro
+    tst DMACONR(a6)                         ;for compatibility
+    btst #6,DMACONR(a6)
+    bne.s *-6
+    endm
+
+*******************************************************************************
+* GAME
+*******************************************************************************
+
+Init:
+    movem.l d0-a6,-(sp)
+
+
+
+    lea Screen,a1
+    bsr.w ClearScreen
+
+    lea Logo,a0                             ;ptr to first bitplane of logo
+    lea CopBplP,a1                          ;where to poke the bitplane pointer words.
+    move #3-1,d0
+.bpll:
+    move.l a0,d1
+    swap d1
+    move.w d1,2(a1)                         ;hi word
+    swap d1
+    move.w d1,6(a1)                         ;lo word
+
+    addq #8,a1                              ;point to next bpl to poke in copper
+    lea logobpl(a0),a0
+    dbf d0,.bpll
+
+
+
+    movem.l (sp)+,d0-a6
+    rts
+
+*******************************************************************************
+* START
+*******************************************************************************
+
 StartGame:
+    bsr.w Init
 
-.lmb 
-          btst #6,$bfe001
-          bne.s .lmb
+    lea $DFF000,a6
+    move.w #$87C0,DMACON(a6)                ;SET+BLTPRI+DMAEN+BPLEN+COPEN+BLTEN
 
-          moveq #0,d0
-          rts
+    move.l #Copper,COP1LCH(a6)
+    move.l #VBint,$6c(a4)                   ;set vertb interrupt vector compatibly.
+    move.w #$c020,INTENA(a6)                ;enable interrupts generally
+                                            ;and vertb specifically.
+
+
+    bsr.s Main
+
+    rts
+
+*******************************************************************************
+* MAIN
+*******************************************************************************
+
+Main:
+    movem.l d0-a6,-(sp)
+
+.WaitMouse
+    btst #6,$bfe001
+    bne.s .WaitMouse
+
+    movem.l (sp)+,d0-a6
+    rts
+
+*******************************************************************************
+* ROUTINES
+*******************************************************************************
+
+ClearScreen:                    			;a1=screen destination address to clear
+    WAITBLIT
+    clr.w $66(a6)               			;destination modulo
+    move.l #$01000000,$40(a6)   			;set operation type in BLTCON0/1
+    move.l a1,$54(a6)           			;destination address
+    move.w #h*bpls*64+bpl/2,$58(a6) 		;blitter operation size
+    rts
+
+VBint:                      				;Blank template VERTB interrupt
+    movem.l d0-a6,-(sp)     				;Save used registers
+    lea $DFF000,a6
+    btst #5,INTREQR(a6)     				;INTREQR check if it's our vertb int.
+    beq.s .notvb
+
+    moveq #$20,d0            				;poll irq bit
+    move.w d0,INTREQ(a6)
+    move.w d0,INTREQ(a6)
+
+.notvb:
+    movem.l (sp)+,d0-a6      				;restore
+    rte
+
+    even
+
+*******************************************************************************
+* DATA (FASTMEM)
+*******************************************************************************
+
+SkyBufferL:
+    dc.l 0
+    dc.l 0
+SkyBufferLE:
+
+*******************************************************************************
+* CHIPMEM
+*******************************************************************************
+
+    SECTION AllData,DATA_C
+
+Copper:
+    dc.w $1fc,0         					;slow fetch mode, AGA compatibility
+    dc.w $100,$0200
+    dc.b 0,$8e,$2c,$81
+    dc.b 0,$90,$2c,$c1
+    dc.w $092,$38
+    dc.w $094,$d0
+
+    dc.w $108,logobwid-logobpl
+    dc.w $10a,logobwid-logobpl
+
+    dc.w $102,$20
+    dc.w $104,0
+
+    dc.w $0180,$0fff    					;white BG
+
+    dc.w $19df,$fffe
+
+CopBplP:
+    dc.w $e0,0          					;1
+    dc.w $e2,0
+    dc.w $e4,0          					;2
+    dc.w $e6,0
+    dc.w $e8,0          					;3
+    dc.w $ea,0
+;   dc.w $ec,0          					;4
+;   dc.w $ee,0
+;   dc.w $f0,0          					;5
+;   dc.w $f2,0
+;   dc.w $f4,0          					;6
+;   dc.w $f6,0
+
+    dc.w $0180,logobgcol
+    dc.w $100,$3200
+    dc.w $2c07,$fffe
+
+LogoPal:
+    dc.w $0180,$044f,$0182,$068e,$0184,$0adf,$0186,$0dff
+    dc.w $0188,$09bf,$018a,$056d,$018c,$044b,$018e,$033a
+
+    dc.w $ffff,$fffe
+CopperE:
+
+Logo:   INCBIN "gfx/sky3centered.raw"
+
+
+EVEN
+
+*******************************************************************************
+* BUFFERS
+*******************************************************************************
+
+    SECTION AllBuffers,BSS_C
+	
+Screen:
+    ds.b bplsize*3
+ScreenE:
+
+
