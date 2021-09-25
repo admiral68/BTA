3b03197b218f34ae75c850a1e1ec4be3bfcefa89
chadderack <icbiass@hotmail.com>
2021-09-25 14:06:39 -0600

    Declare space for decoded graphic

--- main.asm
+++ main.asm
@@ -15,16 +15,16 @@ bpl             =w/16*2                 ;byte-width of 1 bitplane line
 bwid            =bpls*bpl               ;byte-width of 1 pixel line (all bpls)
 
 
-om_bp_offset	= $100
-om_src_rows		= 2
-om_src_cols		= 2
-om_tile_b_offs	= $40
-om_upr_px_b_off	= $20
-om_upr_px_offs	= 8
+om_bp_offset    = $100
+om_src_rows     = 2
+om_src_cols     = 2
+om_tile_b_offs  = $40
+om_upr_px_b_off = $20
+om_upr_px_offs  = 8
 
 
     *-----------------*
-	* logo dimensions *
+    * logo dimensions *
     *-----------------*
 
 logow           =320
@@ -33,7 +33,7 @@ logobpl         =logow/8
 logobwid        =logobpl*3
 
     *-----------------*
-	* palettes        *
+    * palettes        *
     *-----------------*
 
 logobgcol       =$44f
@@ -41,7 +41,7 @@ logobgcol       =$44f
 
 
     *-----------------*
-	* registers       *
+    * registers       *
     *-----------------*
 
 
@@ -61,13 +61,13 @@ logocolors:macro
     dc.w $068e,$0adf,$0dff
     dc.w $09bf,$056d,$044b,$033a
     endm
-	
+
 copper_pal_03:macro
     dc.w $0180,$0b87,$0182,$0754,$0184,$0975,$0186,$0ca8
     dc.w $0188,$0ed8,$018a,$0fff,$018c,$0060,$018e,$0090
     dc.w $0190,$00e0,$0192,$0777,$0194,$0aaa,$0196,$0747
     dc.w $0198,$0868,$019a,$0a8a,$019c,$0cac,$019e,$0111
-	endm
+    endm
 
 WAITBLIT:macro
     tst DMACONR(a6)                         ;for compatibility
@@ -101,7 +101,7 @@ Init:
     lea logobpl(a0),a0
     dbf d0,.bpll
 
-	bsr DecodeOldMan
+    bsr DecodeOldMan
 
 
     movem.l (sp)+,d0-a6
@@ -146,286 +146,286 @@ Main:
 *******************************************************************************
 
 Extract8PixelPaletteValues:
-	;INPUT: d1 - contribution	
-	;       d2 - source word (packed bytes)
-	;       a1 - source bytes ptr
-	;       a2 - 8 decoded palette indexes ptr
-	;USES:  d3
-	;OUTPUT: a2 - decoded pixels (8)
-	
-	move.l 0,0(a2)
-	move.l 0,4(a2)
-	move.l 0,8(a2)
-	move.l 0,12(a2)
-	
-	move.l d1,d3
-	
-	cmp.b 1,d2
-	bne .check_2
-	
-	or.w 3(a2),d3			;3
-	move.b d3,3(a2)
-	
+    ;INPUT: d1 - contribution
+    ;       d2 - source word (packed bytes)
+    ;       a1 - source bytes ptr
+    ;       a2 - 8 decoded palette indexes ptr
+    ;USES:  d3
+    ;OUTPUT: a2 - decoded pixels (8)
+
+    move.l 0,0(a2)
+    move.l 0,4(a2)
+    move.l 0,8(a2)
+    move.l 0,12(a2)
+
+    move.l d1,d3
+
+    cmp.b 1,d2
+    bne .check_2
+
+    or.w 3(a2),d3           ;3
+    move.b d3,3(a2)
+
 .check_2
-	move.l 8,d3
-	cmp.b 2,d2
-	bne .check_4
-	
-	or.w 2(a2),d3           ;2
-	move.b d3,2(a2)
+    move.l 8,d3
+    cmp.b 2,d2
+    bne .check_4
+
+    or.w 2(a2),d3           ;2
+    move.b d3,2(a2)
 
 .check_4
-	move.l 8,d3
-	cmp.b 4,d2
-	bne .check_8
-	
-	or.w 1(a2),d3           ;1
-	move.b d3,1(a2)
-	
-.check_8
-	move.l 8,d3
-	cmp.b 8,d2
-	bne .next_byte
+    move.l 8,d3
+    cmp.b 4,d2
+    bne .check_8
 
-	or.w (a2),d3            ;0
-	move.b d3,(a2)
+    or.w 1(a2),d3           ;1
+    move.b d3,1(a2)
+
+.check_8
+    move.l 8,d3
+    cmp.b 8,d2
+    bne .next_byte
+
+    or.w (a2),d3            ;0
+    move.b d3,(a2)
 
 .next_byte
-	
-	swap d2					;swaps source bytes
 
-	move.l d1,d3
-	
-	cmp.b 1,d2
-	bne .check_2a
-	
-	or.w 7(a2),d3           ;7
-	move.b d3,7(a2)
-	
+    swap d2                 ;swaps source bytes
+
+    move.l d1,d3
+
+    cmp.b 1,d2
+    bne .check_2a
+
+    or.w 7(a2),d3           ;7
+    move.b d3,7(a2)
+
 .check_2a
-	move.l 8,d3
-	cmp.b 2,d2
-	bne .check_4a
-	
-	or.w 6(a2),d3           ;6
-	move.b d3,6(a2)
+    move.l 8,d3
+    cmp.b 2,d2
+    bne .check_4a
+
+    or.w 6(a2),d3           ;6
+    move.b d3,6(a2)
 
 .check_4a
-	move.l 8,d3
-	cmp.b 4,d2
-	bne .check_8a
-	
-	or.w 5(a2),d3           ;5
-	move.b d3,5(a2)
-	
-.check_8a
-	move.l 8,d3
-	cmp.b 8,d2
-	bne .next_bytea
+    move.l 8,d3
+    cmp.b 4,d2
+    bne .check_8a
 
-	or.w 4(a2),d3           ;4
-	move.b d3,4(a2)
+    or.w 5(a2),d3           ;5
+    move.b d3,5(a2)
+
+.check_8a
+    move.l 8,d3
+    cmp.b 8,d2
+    bne .next_bytea
+
+    or.w 4(a2),d3           ;4
+    move.b d3,4(a2)
 
 .next_bytea
-	
-	swap d2				;swaps source bytes
 
-	move.l d1,d3
-	asr d3
-	
-	cmp.b $10,d2
-	bne .check_2b
-	
-	or.w 3(a2),d3           ;3
-	move.b d3,3(a2)
-	
+    swap d2             ;swaps source bytes
+
+    move.l d1,d3
+    asr d3
+
+    cmp.b $10,d2
+    bne .check_2b
+
+    or.w 3(a2),d3           ;3
+    move.b d3,3(a2)
+
 .check_2b
-	move.l 8,d3
-	cmp.b $20,d2
-	bne .check_4b
-	
-	or.w 2(a2),d3           ;2
-	move.b d3,2(a2)
+    move.l 8,d3
+    cmp.b $20,d2
+    bne .check_4b
+
+    or.w 2(a2),d3           ;2
+    move.b d3,2(a2)
 
 .check_4b
-	move.l 8,d3
-	cmp.b $40,d2
-	bne .check_8b
-	
-	or.w 1(a2),d3           ;1
-	move.b d3,1(a2)
-	
-.check_8b
-	move.l 8,d3
-	cmp.b $80,d2
-	bne .next_byteb
+    move.l 8,d3
+    cmp.b $40,d2
+    bne .check_8b
 
-	or.w (a2),d3            ;0
-	move.b d3,(a2)
+    or.w 1(a2),d3           ;1
+    move.b d3,1(a2)
+
+.check_8b
+    move.l 8,d3
+    cmp.b $80,d2
+    bne .next_byteb
+
+    or.w (a2),d3            ;0
+    move.b d3,(a2)
 
 .next_byteb
 
-	swap d2				;swaps source bytes
+    swap d2             ;swaps source bytes
 
-	move.l d1,d3
-	asr d3
-	
-	cmp.b 1,d2
-	bne .check_2c
-	
-	or.w 7(a2),d3           ;7
-	move.b d3,7(a2)
-	
+    move.l d1,d3
+    asr d3
+
+    cmp.b 1,d2
+    bne .check_2c
+
+    or.w 7(a2),d3           ;7
+    move.b d3,7(a2)
+
 .check_2c
-	move.l 8,d3
-	cmp.b 2,d2
-	bne .check_4c
-	
-	or.w 6(a2),d3           ;6
-	move.b d3,6(a2)
+    move.l 8,d3
+    cmp.b 2,d2
+    bne .check_4c
+
+    or.w 6(a2),d3           ;6
+    move.b d3,6(a2)
 
 .check_4c
-	move.l 8,d3
-	cmp.b 4,d2
-	bne .check_8c
-	
-	or.w 5(a2),d3           ;5
-	move.b d3,5(a2)
-	
-.check_8c
-	move.l 8,d3
-	cmp.b 8,d2
-	bne .end
+    move.l 8,d3
+    cmp.b 4,d2
+    bne .check_8c
 
-	or.w 4(a2),d3           ;4
-	move.b d3,4(a2)
+    or.w 5(a2),d3           ;5
+    move.b d3,5(a2)
+
+.check_8c
+    move.l 8,d3
+    cmp.b 8,d2
+    bne .end
+
+    or.w 4(a2),d3           ;4
+    move.b d3,4(a2)
 
 .end
-	rts
-	
-	
+    rts
+
+
 DecodeRowOfPixels:
-	;INPUT: a1 - source bytes ptr
-	;USES:  d2
-	;OUTPUT: a2 - decoded pixels (16)	
+    ;INPUT: a1 - source bytes ptr
+    ;USES:  d2
+    ;OUTPUT: a2 - decoded pixels (16)
 
-	move.w om_bp_offset(a1),d2						;om_bp_offset = offset to bitplanes 0 and 1 in source
-	move.w 8,d1
-	lea DecodedPaletteIndexes,a2
-	
+    move.w om_bp_offset(a1),d2                      ;om_bp_offset = offset to bitplanes 0 and 1 in source
+    move.w 8,d1
+    lea DecodedPaletteIndexes,a2
 
-													;Bitplane 01 - lower nybble; Bitplane 00 - upper nybble
-	bsr Extract8PixelPaletteValues					;returns DecodedPaletteIndexes in a2
 
-	move.w (a1),d2
-	move.w 8,d1
-	lea DecodedPaletteIndexes+8,a2
-	
+                                                    ;Bitplane 01 - lower nybble; Bitplane 00 - upper nybble
+    bsr Extract8PixelPaletteValues                  ;returns DecodedPaletteIndexes in a2
 
-													;Bitplane 03 - lower nybble; Bitplane 02 - upper nybble
-	bsr Extract8PixelPaletteValues					;returns DecodedPaletteIndexes in a2
-	rts
-	
-	
+    move.w (a1),d2
+    move.w 8,d1
+    lea DecodedPaletteIndexes+8,a2
+
+
+                                                    ;Bitplane 03 - lower nybble; Bitplane 02 - upper nybble
+    bsr Extract8PixelPaletteValues                  ;returns DecodedPaletteIndexes in a2
+    rts
+
+
 DecodeOldMan:
-	lea Oldguy,a1
-	
+    lea Oldguy,a1
+
     move #om_tile_b_offs/2,d0
 .extract_tile_01:
 
-	bsr DecodeRowOfPixels
-								;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
-								;TODO: here's where we need to check cmp.w $10,d0
+    bsr DecodeRowOfPixels
+                                ;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
+                                ;TODO: here's where we need to check cmp.w $10,d0
 
 
     dbf d0,.extract_tile_01
 
-	move.l $40(a1),(a1)			;a1 += om_tile_b_offs; // Advance to next raw source bytes for next tile
-	
+    move.l $40(a1),(a1)         ;a1 += om_tile_b_offs; // Advance to next raw source bytes for next tile
+
 
     move #om_tile_b_offs/2,d0
 .extract_tile_02:
 
-	bsr DecodeRowOfPixels
-								;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
-								;TODO: here's where we need to check cmp.w $10,d0
+    bsr DecodeRowOfPixels
+                                ;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
+                                ;TODO: here's where we need to check cmp.w $10,d0
 
     dbf d0,.extract_tile_02
 
-	move.l $40(a1),(a1)			;a1 += om_tile_b_offs; // Advance to next raw source bytes for next tile
+    move.l $40(a1),(a1)         ;a1 += om_tile_b_offs; // Advance to next raw source bytes for next tile
 
 
 
     move #om_tile_b_offs/2,d0
 .extract_tile_03:
 
-	bsr DecodeRowOfPixels
-								;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
-								;TODO: here's where we need to check cmp.w $10,d0
+    bsr DecodeRowOfPixels
+                                ;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
+                                ;TODO: here's where we need to check cmp.w $10,d0
 
     dbf d0,.extract_tile_03
 
-	move.l $40(a1),(a1)			;a1 += om_tile_b_offs; // Advance to next raw source bytes for next tile
+    move.l $40(a1),(a1)         ;a1 += om_tile_b_offs; // Advance to next raw source bytes for next tile
 
 
 
     move #om_tile_b_offs/2,d0
 .extract_tile_04:
 
-	bsr DecodeRowOfPixels
-								;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
-								;TODO: here's where we need to check cmp.w $10,d0
+    bsr DecodeRowOfPixels
+                                ;TODO: PUT RETURNED PALETTE INDEXES INTO NEW BITMAP
+                                ;TODO: here's where we need to check cmp.w $10,d0
 
     dbf d0,.extract_tile_04
-	
-	
-	
-	
-;om_bp_offset	= $100
-;om_src_rows		= 2
-;om_src_cols		= 2
-;om_tile_b_offs	= $40
-;om_upr_px_b_off	= $20
-;om_upr_px_offs	= 8	
 
 
 
 
-	;cmp.w $10,d0				;var pxOffset = d0 < (om_upr_px_b_off / 2) ? 0 : om_upr_px_offs; 
-	;ble	.part2					;pixel columns 0-7 are not offset; 
-	
-	;pxOffset = om_upr_px_offs;
-	
-	;pixel columns 8-F are offset by upperPixelsByteOffset
-	
+;om_bp_offset   = $100
+;om_src_rows        = 2
+;om_src_cols        = 2
+;om_tile_b_offs = $40
+;om_upr_px_b_off    = $20
+;om_upr_px_offs = 8
+
+
+
+
+    ;cmp.w $10,d0               ;var pxOffset = d0 < (om_upr_px_b_off / 2) ? 0 : om_upr_px_offs;
+    ;ble    .part2                  ;pixel columns 0-7 are not offset;
+
+    ;pxOffset = om_upr_px_offs;
+
+    ;pixel columns 8-F are offset by upperPixelsByteOffset
+
 ;.part2
 
 
 
 
 
-	rts
-
-ClearScreen:                    			;a1=screen destination address to clear
-    WAITBLIT
-    clr.w $66(a6)               			;destination modulo
-    move.l #$01000000,$40(a6)   			;set operation type in BLTCON0/1
-    move.l a1,$54(a6)           			;destination address
-    move.w #h*bpls*64+bpl/2,$58(a6) 		;blitter operation size
     rts
 
-VBint:                      				;Blank template VERTB interrupt
-    movem.l d0-a6,-(sp)     				;Save used registers
+ClearScreen:                                ;a1=screen destination address to clear
+    WAITBLIT
+    clr.w $66(a6)                           ;destination modulo
+    move.l #$01000000,$40(a6)               ;set operation type in BLTCON0/1
+    move.l a1,$54(a6)                       ;destination address
+    move.w #h*bpls*64+bpl/2,$58(a6)         ;blitter operation size
+    rts
+
+VBint:                                      ;Blank template VERTB interrupt
+    movem.l d0-a6,-(sp)                     ;Save used registers
     lea $DFF000,a6
-    btst #5,INTREQR(a6)     				;INTREQR check if it's our vertb int.
+    btst #5,INTREQR(a6)                     ;INTREQR check if it's our vertb int.
     beq.s .notvb
 
-    moveq #$20,d0            				;poll irq bit
+    moveq #$20,d0                           ;poll irq bit
     move.w d0,INTREQ(a6)
     move.w d0,INTREQ(a6)
 
 .notvb:
-    movem.l (sp)+,d0-a6      				;restore
+    movem.l (sp)+,d0-a6                     ;restore
     rte
 
     even
@@ -440,11 +440,7 @@ SkyBufferL:
 SkyBufferLE:
 
 DecodedPaletteIndexes:
-	dc.b 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-
-NibbleReverseLut:
-	dc.b $00, $08, $04, $0C, $02, $0A, $06, $0E 
-	dc.b $01, $09, $05, $0D, $03, $0B, $07, $0F
+    dc.b 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 *******************************************************************************
 * CHIPMEM
@@ -453,7 +449,7 @@ NibbleReverseLut:
     SECTION AllData,DATA_C
 
 Copper:
-    dc.w $01fc,0         					;slow fetch mode, AGA compatibility
+    dc.w $01fc,0                            ;slow fetch mode, AGA compatibility
     dc.w $0100,$0200
     dc.b 0,$8e,$2c,$81
     dc.b 0,$90,$2c,$c1
@@ -474,23 +470,23 @@ Copper:
 
 
 CopBplP:
-    dc.w $00e0,0          					;1
+    dc.w $00e0,0                            ;1
     dc.w $00e2,0
-    dc.w $00e4,0          					;2
+    dc.w $00e4,0                            ;2
     dc.w $00e6,0
-    dc.w $00e8,0          					;3
+    dc.w $00e8,0                            ;3
     dc.w $00ea,0
-;   dc.w $00ec,0          					;4
+;   dc.w $00ec,0                            ;4
 ;   dc.w $00ee,0
-;   dc.w $00f0,0          					;5
+;   dc.w $00f0,0                            ;5
 ;   dc.w $00f2,0
-;   dc.w $00f4,0          					;6
+;   dc.w $00f4,0                            ;6
 ;   dc.w $00f6,0
 
 
     ;dc.w $0180,logobgcol
-	
-	
+
+
     dc.w $0100,$3200
     dc.w $4a07,$fffe
 
@@ -499,7 +495,7 @@ CopBplP:
     dc.w $0190,$00e0,$0192,$0777,$0194,$0aaa,$0196,$0747
     dc.w $0198,$0868,$019a,$0a8a,$019c,$0cac,$019e,$0111
 
-	dc.w $9207,$fffe
+    dc.w $9207,$fffe
 
     dc.w $0180,$044f,$0182,$068e,$0184,$0adf,$0186,$0dff
     dc.w $0188,$09bf,$018a,$056d,$018c,$044b,$018e,$033a
@@ -508,11 +504,11 @@ CopBplP:
 CopperE:
 
 Oldguy: INCBIN "gfx/testgrfx.bin"
-	dcb.b logobwid*6,0
+    dcb.b logobwid*6,0
 
 Logo:   INCBIN "gfx/sky3centered.raw"
 LogoE:
-	dcb.b logobwid*6,0
+    dcb.b logobwid*6,0
 
 EVEN
 
@@ -521,9 +517,13 @@ EVEN
 *******************************************************************************
 
     SECTION AllBuffers,BSS_C
-	
+
 Screen:
     ds.b bplsize*3
 ScreenE:
+
+DecodedGraphic:
+    ds.b 384            ;Define storage for graphic
+DecodedGraphicE:
 
 
