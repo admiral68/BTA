be574b84ff683f535b6a28028c89317c3634c9f5
chadderack <icbiass@hotmail.com>
2021-09-28 04:02:20 -0600

    Decode entire old man from real sprite sheet.

--- main.asm
+++ main.asm
@@ -13,16 +13,16 @@ bplsize                     = w*h/8
 tile_bitplanes              = 4
 tile_height                 = 16
 
+tilesrc_row_w               = $200
+tilesrc_bp_offset           = $20000
+tilesrc_upr_px_b_off        = $20
+
 bitpl_bytes_per_raster_line = 40
 
 bpls                        = 3                             ;handy values:
 bpl                         = w/16*2                        ;byte-width of 1 bitplane line
 
-om_bp_offset                = $100
-om_upr_px_b_off             = $20
-
-
-test_vlines_per_graphic     = 32
+test_vlines_per_graphic     = 48                            ;32
 
     *-----------------*
     * logo dimensions *
@@ -67,10 +67,10 @@ tile_pal_01:macro
     endm
 
 tile_pal_02:macro
-    dc.w $0180,$0xxx,$0182,$0xxx,$0184,$0xxx,$0186,$0xxx
-    dc.w $0188,$0xxx,$018a,$0xxx,$018c,$0xxx,$018e,$0xxx
-    dc.w $0190,$0xxx,$0192,$0xxx,$0194,$0xxx,$0196,$0xxx
-    dc.w $0198,$0xxx,$019a,$0xxx,$019c,$0xxx,$019e,$0xxx
+    dc.w $0180,$0b87,$0182,$0544,$0184,$0754,$0186,$0975
+    dc.w $0188,$0ca8,$018a,$0eea,$018c,$0fc4,$018e,$0653
+    dc.w $0190,$0974,$0192,$0c84,$0194,$0eef,$0196,$0aaa
+    dc.w $0198,$0889,$019a,$0778,$019c,$0556,$019e,$0111
     endm
 
 tile_pal_03:macro
@@ -355,17 +355,19 @@ Extract8BitplaneBytesFromTwoSourceBytes:
 
 DecodeRowOf16Pixels:
     ;INPUT: a1 - source bytes ptr
-    ;USES:  d2
+    ;USES:  d2,a4
     ;OUTPUT: a2 - bitplane data
     ;        a3 - destination
 
     move.l 0,0(a2)                                          ;leftmost column destinations
     move.l 0,4(a2)                                          ;rightmost column destinations
 
+    move.l a1,d2
+    add.l #tilesrc_bp_offset,d2                             ;tilesrc_bp_offset = offset to bitplanes 0 and 1 in source
+    move.l d2,a4
 
                                                             ;leftmost columns of 8 pixels
-
-    move.w om_bp_offset(a1),d2                              ;om_bp_offset = offset to bitplanes 0 and 1 in source
+    move.w (a4),d2
                                                             ;d2.w = zeroAndOneByte1 & zeroAndOneByte2
                                                             ;a2 => Bitplane 01 - lower byte; Bitplane 00 - upper byte
     bsr Extract8BitplaneBytesFromTwoSourceBytes             ;returns DecodedBitplaneBytes in a2
@@ -379,14 +381,14 @@ DecodeRowOf16Pixels:
                                                             ;rightmost columns of 8 pixels
 
     lea 2(a2),a2
-    move.w om_bp_offset+om_upr_px_b_off(a1),d2              ;add $20 to get to the src of the rightmost 8 pixel columns
+    move.w tilesrc_upr_px_b_off(a4),d2                      ;add $20 to get to the src of the rightmost 8 pixel columns
                                                             ;d2.w = zeroAndOneByte1 & zeroAndOneByte2
                                                             ;a2 => Bitplane 01 - lower byte; Bitplane 00 - upper byte
 
     bsr Extract8BitplaneBytesFromTwoSourceBytes
 
     lea 2(a2),a2
-    move.w om_upr_px_b_off(a1),d2                           ;d2.w = twoAndThreeByte1 & twoAndThreeByte2
+    move.w tilesrc_upr_px_b_off(a1),d2                      ;d2.w = twoAndThreeByte1 & twoAndThreeByte2
                                                             ;a2 => Bitplane 03 - lower byte; Bitplane 02 - upper byte
 
     bsr Extract8BitplaneBytesFromTwoSourceBytes
@@ -409,8 +411,12 @@ DecodeOldMan:
     clr.l (a0)+
     dbf d0,.l0
 
+    move.l #$000002E9,d0                                    ;Starting tile
+    asl.l #$06,d0
+
     lea DecodedGraphic,a3
-    lea Oldguy,a1
+    lea EncTiles,a1
+    lea (a1,d0.l),a1                                        ;Oldguy
 
     ; ******************************************************
 
@@ -463,7 +469,11 @@ DecodeOldMan:
 
     ; ******************************************************
 
-    lea $20(a1),a1                                          ;skip to next tile in the source
+    move.l #$000002F1,d0                                    ;skip to next tile in the source
+    asl.l #$06,d0
+    lea EncTiles,a1
+    lea (a1,d0.l),a1
+
     lea DecodedGraphic+(bitpl_bytes_per_raster_line*tile_bitplanes*tile_height),a3
     move #$0F,d0                                            ;16 rasterlines at a time
 .extract_tile_03:
@@ -508,6 +518,59 @@ DecodeOldMan:
     lea $02(a1),a1                                          ;source bytes per rasterline are 2 bytes apart
 
     dbf d0,.extract_tile_04
+
+    ; ******************************************************
+
+    move.l #$000002F9,d0                                    ;skip to next tile in the source
+    asl.l #$06,d0
+    lea EncTiles,a1
+    lea (a1,d0.l),a1
+
+    lea DecodedGraphic+(bitpl_bytes_per_raster_line*tile_bitplanes*tile_height*2),a3
+    move #$0F,d0                                            ;16 rasterlines at a time
+.extract_tile_05:
+
+    lea DecodedBitplaneBytes,a2
+    bsr DecodeRowOf16Pixels
+
+    move.b, 3(a2),bitpl_bytes_per_raster_line*0(a3)         ;bitplane 0
+    move.b, 2(a2),bitpl_bytes_per_raster_line*1(a3)         ;bitplane 1
+    move.b, 1(a2),bitpl_bytes_per_raster_line*2(a3)         ;bitplane 2
+    move.b, (a2),bitpl_bytes_per_raster_line*3(a3)          ;bitplane 3
+    move.b, 7(a2),bitpl_bytes_per_raster_line*0+1(a3)       ;bitplane 0
+    move.b, 6(a2),bitpl_bytes_per_raster_line*1+1(a3)       ;bitplane 1
+    move.b, 5(a2),bitpl_bytes_per_raster_line*2+1(a3)       ;bitplane 2
+    move.b, 4(a2),bitpl_bytes_per_raster_line*3+1(a3)       ;bitplane 3
+
+    lea bitpl_bytes_per_raster_line*tile_bitplanes(a3),a3   ;move down one rasterline
+    lea $02(a1),a1                                          ;source bytes per rasterline are 2 bytes apart
+
+    dbf d0,.extract_tile_05
+
+    ; ******************************************************
+
+    lea $20(a1),a1                                          ;skip to next tile in the source
+    lea DecodedGraphic+(bitpl_bytes_per_raster_line*tile_bitplanes*tile_height*2)+2,a3
+    move #$0F,d0                                            ;16 rasterlines at a time
+.extract_tile_06:
+
+    lea DecodedBitplaneBytes,a2
+    bsr DecodeRowOf16Pixels
+
+    move.b, 3(a2),bitpl_bytes_per_raster_line*0(a3)         ;bitplane 0
+    move.b, 2(a2),bitpl_bytes_per_raster_line*1(a3)         ;bitplane 1
+    move.b, 1(a2),bitpl_bytes_per_raster_line*2(a3)         ;bitplane 2
+    move.b, (a2),bitpl_bytes_per_raster_line*3(a3)          ;bitplane 3
+    move.b, 7(a2),bitpl_bytes_per_raster_line*0+1(a3)       ;bitplane 0
+    move.b, 6(a2),bitpl_bytes_per_raster_line*1+1(a3)       ;bitplane 1
+    move.b, 5(a2),bitpl_bytes_per_raster_line*2+1(a3)       ;bitplane 2
+    move.b, 4(a2),bitpl_bytes_per_raster_line*3+1(a3)       ;bitplane 3
+
+    lea bitpl_bytes_per_raster_line*tile_bitplanes(a3),a3   ;move down one rasterline
+    lea $02(a1),a1                                          ;source bytes per rasterline are 2 bytes apart
+
+    dbf d0,.extract_tile_06
+
     rts
 ;-----------------------------------------------
 
@@ -543,17 +606,13 @@ VBint:                                                      ;Blank template VERT
 
 DecodedBitplaneBytes:
     dc.b 0,0,0,0,0,0,0,0
-	
-	EVEN
-
-Oldguy: INCBIN "gfx/testgrfx.bin"
-
-	EVEN
-	
-EncTiles: INCBIN "gfx/tile_data.bin"
 
     EVEN
-	
+
+EncTiles: INCBIN "gfx/gfx2.bin"
+
+    EVEN
+
 
 *******************************************************************************
 * CHIPMEM
